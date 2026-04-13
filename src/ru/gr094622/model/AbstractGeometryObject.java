6198ae2a4dae0adbc0871eaf57f9baaaf97d4d32
Beytullah Gadzhimirzoev <gaqjimirzoevbeytullah@gmail.com>
2026-04-13 13:08:50 +0300

    feature: paint method implementation

--- src/ru/gr094622/model/AbstractGeometryObject.java
+++ src/ru/gr094622/model/AbstractGeometryObject.java
@@ -1,8 +1,58 @@
 package ru.gr094622.model;
 
 import java.awt.*;
-import java.util.Random;
 
-public abstract class AbstractGeometryObject implements GeometryObject{
+public abstract class AbstractGeometryObject implements GeometryObject {
 
+    private int x;
+    private int y;
+    private Dimension size;
+    private Color color;
+    private int xSpeed;
+    private int ySpeed;
+
+    public AbstractGeometryObject(int x, int y, Dimension size, Color color, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.color = color;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    @Override
+    public int getX() { return x; }
+
+    @Override
+    public void setX(int x) { this.x = x; }
+
+    @Override
+    public int getY() { return y; }
+
+    @Override
+    public void setY(int y) { this.y = y; }
+
+    @Override
+    public Dimension getSize() { return size; }
+
+    @Override
+    public void setSize(Dimension size) { this.size = size; }
+
+    @Override
+    public Color getColor() { return color; }
+
+    @Override
+    public int getXSpeed() { return xSpeed; }
+
+    @Override
+    public int getYSpeed() { return ySpeed; }
+
+    @Override
+    public void inverseXSpeed() { xSpeed = -xSpeed; }
+
+    @Override
+    public void inverseYSpeed() { ySpeed = -ySpeed; }
+
+    @Override
+    public abstract void paint(Graphics g);
 }
--- src/ru/gr094622/painting/Rect.java
+++ src/ru/gr094622/painting/Rect.java
@@ -2,5 +2,17 @@ package ru.gr094622.painting;
 
 import ru.gr094622.model.AbstractGeometryObject;
 
+import java.awt.*;
+
 public class Rect extends AbstractGeometryObject {
+
+    public Rect(int x, int y, Dimension size, Color color, int xSpeed, int ySpeed) {
+        super(x, y, size, color, xSpeed, ySpeed);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(getColor());
+        g.fillRect(getX(), getY(), getSize().width, getSize().height);
+    }
 }
